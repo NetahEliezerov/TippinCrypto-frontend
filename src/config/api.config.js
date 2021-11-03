72ea7fa084dc178657da9fe9e9717ea14d15fbc0
Netah Eliezerov <65071328+NetahEliezerov@users.noreply.github.com>
2021-11-03 18:38:17 +0200

    Update api.config.js

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,7 +1,7 @@
 export const apiConfig = {
     "urls": {
         "mainNetWeb3Provider": "https://mainnet.infura.io/v3/1be468ea59274a5ea108fd6cd8df58cf",
-        "backendService": "http://localhost:5050",
+        "backendService": "http://153.92.222.12:443",
         "domain": "tippincrypto.com"
     },
     "modes": {
